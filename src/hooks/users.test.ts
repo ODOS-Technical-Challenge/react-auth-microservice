e500e063e5293cb2f505dba90b6c5ca749220b22
Clayton Miller <cmiller@synergybis.com>
2022-03-02 12:52:34 -0500

    remvoed duplicate test

--- src/hooks/users.test.ts
+++ src/hooks/users.test.ts
@@ -8,26 +8,17 @@ import { useUsers } from "./users.hook";
 jest.mock("../api", () => ({
   UserApi: { getAll: jest.fn() },
 }));
-
 jest.mock("react-router-dom");
 
 const getAll = UserApi.getAll as jest.MockedFunction<typeof UserApi.getAll>;
 
-describe("User Hook", () => {
+describe("Hook: Use Users", () => {
   beforeEach(() => {
     getAll.mockClear();
 
     (
       useSearchParams as jest.MockedFunction<typeof useSearchParams>
     ).mockReturnValue([new URLSearchParams(""), jest.fn()]);
-  });
-
-  it("should handle being called.", () => {
-    getAll.mockResolvedValue({ data: [], status: 200 });
-    const { result } = renderHook(() => useUsers());
-
-    expect(result.current.isLoading).toBeTruthy();
-    expect(result.current.data).toEqual([]);
   });
 
   it("should handle resolving REST call.", () => {
@@ -53,5 +44,6 @@ describe("User Hook", () => {
     const { result } = renderHook(() => useUsers());
 
     expect(result.current.isLoading).toBeTruthy();
+    // expect(result.current.data).toEqual([user]);
   });
 });
